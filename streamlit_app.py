6e70c042cd64ea4d9b8735fd5089739527efd547
abdusalam7474 <mideola824@gmail.com>
2023-04-15 19:52:33 +0100

    app with refresh radio

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from tensorflow.keras.preprocessing.text import tokenizer_from_json
 #import streamlit as st
 #from streamlit.state.session_state import SessionState
 
-session_state = st.sessionstate.get(form_mode=None)
+session_state = st.session_state.get(form_mode=None)
 
 # Load the model
 model = load_model('my_model.h5')
