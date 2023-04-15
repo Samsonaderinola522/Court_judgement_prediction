41ab5d82c051b8434a48aee66aeae2d91d489396
abdusalam7474 <mideola824@gmail.com>
2023-04-15 14:25:53 +0100

    test randome update 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,16 @@ def main():
     # Add a text input
     number_input = st.text_input('Enter a number:')
     
+    # Add a text input
+    number_input_new = st.text_input('updated')
+    
     # Add a button that generates a random number
     if st.button('Generate Random Number'):
         # Generate a random integer between 1 and 10
         random_number = random.randint(1, 10)
         
         # Update the value of the text input with the generated number
-        st.session_state.number_input = str(random_number)
+        st.session_state.number_input_new = str(random_number)
 
 if __name__ == '__main__':
     main()
