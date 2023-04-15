9dacbc8383b4990208790a3742501b6933c2be57
abdusalam7474 <mideola824@gmail.com>
2023-04-15 14:28:52 +0100

    test randome update 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import random
 
 def main():
     st.title('Random Number Generator')
-    
+    cvu = "x"
     # Add a text input
     number_input = st.text_input('Enter a number:')
     
     # Add a text input
-    number_input_new = st.text_input('updated')
+    number_input_new = st.text_input('updated', value=cvu)
     
     # Add a button that generates a random number
     if st.button('Generate Random Number'):
@@ -16,7 +16,7 @@ def main():
         random_number = random.randint(1, 10)
         
         # Update the value of the text input with the generated number
-        st.session_state.number_input_new = str(random_number)
+        st.session_state.cvu = str(random_number)
 
 if __name__ == '__main__':
     main()
